07a6425f5a5b073b22da1ffed1f99bcb0e705a5f
EmmittJ <1337ERJ1337@gmail.com>
2018-12-07 22:27:32 -0800

    whoops, commented out the wrong line

--- app/app.ts
+++ app/app.ts
@@ -104,8 +104,8 @@ namespace App {
             node_graphic.scale.set(2.75);
             node_graphic.x = node.x;
             node_graphic.y = node.y;
-            //viewport.addChild(node_graphic);
-            viewport.addChild(node.getGraphic());
+            viewport.addChild(node_graphic);
+            //viewport.addChild(node.getGraphic());
         }
     }
 }
